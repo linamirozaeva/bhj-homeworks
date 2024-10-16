b88da52763c72e1bc9256942ee8c3b176771b93f
karolina <karolinavasko@gmail.com>
2024-10-16 22:23:41 +0300

    1

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,16 +1,23 @@
-// const reveals = document.querySelectorAll('.reveal');
+const reveals = document.querySelectorAll('.reveal');
 
-// reveals.getBoundingClientRect() => {
-//     if 
-// }
+window.addEventListener('scroll', function(){
+    reveals.innerHeight;
+    reveals.classList.toggle('reveal_active');
 
+    // const elementHeight = reveals.getBoundingClientRect().height;
+    // const windowHeight = window.innerHeight;
+    // if (elementHeight < windowHeight){
+    //     reveals.classList.toggle('reveal_active');
+    // }
+});
+isInViewport(reveals);
 
-let isInViewport = function(reveals){
-    const viewportHeight = window.innerHeight;
-    const elementHeight = reveals.getBoundingClientRect().height;
-    if (elementHeight < viewportHeight){
-        reveals.classList.add('reveal_active');
-    }
-};
-const div = document.querySelector('.reveal');
-isInViewport(div);
+// let isInViewport = function(reveals){
+//     const viewportHeight = window.innerHeight;
+//     const elementHeight = reveals.getBoundingClientRect().height;
+//     if (elementHeight < viewportHeight){
+//         reveals.classList.add('reveal_active');
+//     }
+// };
+// const div = document.querySelector('.reveal');
+// isInViewport(div);
