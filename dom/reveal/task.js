a8e65043693a4fda859031d3c6e5286a7d108a75
karolina <karolinavasko@gmail.com>
2024-10-20 14:15:15 +0300

    initial commit

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -21,16 +21,3 @@ window.addEventListener('scroll', function(){
     // }
 // });
 // isInViewport(reveals);
-
-
-
-
-// let isInViewport = function(reveals){
-//     const viewportHeight = window.innerHeight;
-//     const elementHeight = reveals.getBoundingClientRect().height;
-//     if (elementHeight < viewportHeight){
-//         reveals.classList.add('reveal_active');
-//     }
-// };
-// const div = document.querySelector('.reveal');
-// isInViewport(div);
