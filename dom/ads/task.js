4303e9aaef96ece29e2b5549bdb01adf1c051ea1
karolina <karolinavasko@gmail.com>
2024-10-21 16:08:13 +0300

    init commit

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -2,11 +2,11 @@ const rotatorCase = Array.from(document.querySelectorAll('.rotator__case'));
 let i = 0;
 
 function changeText(){
+        i++;
+        rotatorCase[i-1].classList.toggle('rotator__case_active');
         if (i == rotatorCase.length){
             i = 0;
         }
-        i++;
-        rotatorCase[i-1].classList.toggle('rotator__case_active');
         rotatorCase[i].classList.toggle('rotator__case_active');
     }
 
--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -3,16 +3,20 @@ const big = document.querySelector('.font-size_big');
 const texts = document.querySelector('.book__content');
 const fontSizes = Array.from(document.querySelectorAll('.font-size'));
 
-small.addEventListener('click', () => {
+small.addEventListener('click', (event) => {
     small.classList.add('font-size_active');
     texts.classList.add('font-size_small');
     fontSizes[1].classList.remove('font-size_active');
+    big.classList.remove('font-size_active');
+    texts.classList.remove('font-size_big');
     event.preventDefault()
 });
 
-big.addEventListener('click', () => {
+big.addEventListener('click', (event) => {
     big.classList.add('font-size_active');
     texts.classList.add('font-size_big');
     fontSizes[1].classList.remove('font-size_active');
+    small.classList.remove('font-size_active');
+    texts.classList.remove('font-size_small');
     event.preventDefault()
 });
